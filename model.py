7e08b6e9fec7ee8dd2561ee2e58943e9e89105a8
nithin9000 <nithinkrishnalal303@gmail.com>
2024-10-25 02:35:40 +0530

    torch

--- model.py
+++ model.py
@@ -2,11 +2,3 @@
 import numpy as np
 import pandas as pd
 
-import tensorflow as tf
-import keras
-import tensorflow.keras.layers as L
-from tensorflow.keras.models import Model
-
-from densenet import Densenet
-
-from tensorflow import keras 
